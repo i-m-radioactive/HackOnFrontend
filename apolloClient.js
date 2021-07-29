847138098d894170baa57ce3909dd847b318948b
i-m-radioactive <anupgupta2312@gmail.com>
2021-07-30 01:49:15 +0530

    refresh

--- apolloClient.js
+++
@@ -1,9 +0,0 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
-
-const apollo = new ApolloClient({
-    uri: 'http://localhost:1337/mutation',
-    cache: new InMemoryCache(),
-    credentials: 'include'
-})
-
-export default apollo;
